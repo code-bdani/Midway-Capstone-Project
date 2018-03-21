7f61fca7cf48a26ec0ed05d6481789c94cb01e49
Brittany D. Smith <bdaniphotography@gmail.com>
2018-03-21 14:10:27 -0500

    Created new file for firebase call & installed jquery as a dev dependency

---
+++ js/firebase-call.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const firebase = "https://ingrid-derrickson-website.firebaseio.com/";
+console.log("firebase", firebase);
+
+console.log("firebase-call.js present and accounted for.");
